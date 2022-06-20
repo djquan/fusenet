5571093afad7f42d3033d7a5b365b10401ea3035
Dan Quan <dan@quan.io>
2022-06-20 11:55:22 -0700

    Refactor

--- lib/nzb/nzb.ex
+++ lib/nzb/nzb.ex
@@ -5,40 +5,42 @@ defmodule Fusenet.Nzb do
   def parse(input) do
     xml = input |> xpath(~x"//nzb")
 
-    meta = %{
-      title: xml |> xpath(~x"./head/meta[@type='title']/text()"s),
-      password: xml |> xpath(~x"./head/meta[@type='password']/text()"s),
-      tag: xml |> xpath(~x"./head/meta[@type='tag']/text()"s),
-      category: xml |> xpath(~x"./head/meta[@type='category']/text()"s)
-    }
-
-    files =
-      xml
-      |> xpath(~x"./file"l)
-      |> Enum.map(fn li ->
-        segments =
-          li
-          |> xpath(~x"./segments/segment"l)
-          |> Enum.map(fn li2 ->
-            %{
-              path: li2 |> xpath(~x"./text()"s),
-              bytes: li2 |> xpath(~x"./@bytes"i),
-              number: li2 |> xpath(~x"./@number"i)
-            }
-          end)
-
-        %{
-          subject: li |> xpath(~x"./@subject"s),
-          poster: li |> xpath(~x"./@poster"s),
-          date: li |> xpath(~x"./@date"s),
-          segments: segments,
-          groups: li |> xpath(~x"groups/group/text()"ls)
-        }
-      end)
-
     %{
-      head: meta,
-      files: files
+      head: xml |> xpath(~x"./head") |> extract_headers(),
+      files: xml |> xpath(~x"./file"l) |> extract_files()
     }
+  end
+
+  defp extract_headers(header) do
+    %{
+      title: header |> xpath(~x"./meta[@type='title']/text()"s),
+      password: header |> xpath(~x"./meta[@type='password']/text()"s),
+      tag: header |> xpath(~x"./meta[@type='tag']/text()"s),
+      category: header |> xpath(~x"./meta[@type='category']/text()"s)
+    }
+  end
+
+  defp extract_files(files) do
+    files
+    |> Enum.map(fn li ->
+      segments =
+        li
+        |> xpath(~x"./segments/segment"l)
+        |> Enum.map(fn li2 ->
+          %{
+            path: li2 |> xpath(~x"./text()"s),
+            bytes: li2 |> xpath(~x"./@bytes"i),
+            number: li2 |> xpath(~x"./@number"i)
+          }
+        end)
+
+      %{
+        subject: li |> xpath(~x"./@subject"s),
+        poster: li |> xpath(~x"./@poster"s),
+        date: li |> xpath(~x"./@date"s),
+        segments: segments,
+        groups: li |> xpath(~x"groups/group/text()"ls)
+      }
+    end)
   end
 end
